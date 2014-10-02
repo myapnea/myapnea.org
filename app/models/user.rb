501f83d4f38fe6bca0f05eec37952a0d4d16f111
pmanko <pmankowski@partners.org>
2014-10-02 17:31:01 -0400

    Devise fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   # Model Validation
   validates_presence_of :first_name, :last_name, :zip_code, :year_of_birth
