1e979a4c64d45d59de61c76f0210783b5946558d
Piotr Mankowski <pmankowski@partners.org>
2014-11-14 17:24:56 -0500

    Closed myapnea/www_myapnea_org#103

--- app/controllers/health_data_controller.rb
+++ app/controllers/health_data_controller.rb
@@ -1,8 +1,9 @@
+
 class HealthDataController < ApplicationController
   before_action :authenticate_user!, :only => [:data_explore, :data_reports, :medications]
   before_action :set_active_top_nav_link_to_health_data
-  before_action :validate_oodt_module, :only => [:explore, :reports]
-  before_action :validate_validic_module, :only => []
+  # before_action :validate_oodt_module, :only => [:explore, :reports]
+  # before_action :validate_validic_module, :only => []
 
   layout "community"
 
