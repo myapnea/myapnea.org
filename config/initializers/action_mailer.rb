5808aa5feefe0f1f66f31b3ad4309a498c72721d
Remo Mueller <remosm@gmail.com>
2014-12-03 15:53:17 -0500

    Removed domain and starttls from action mailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,10 +2,8 @@ ActionMailer::Base.default_url_options[:host] = ENV['website_url'].to_s.gsub(/^h
 unless Rails.env.test?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    enable_starttls_auto: true,
     address: ENV['smtp_address'],
     port: ENV['smtp_port'].to_i,
-    domain: (ENV['smtp_domain'].blank? ? nil : ENV['smtp_domain']),
     authentication: ENV['smtp_authentication'].to_sym, # :plain, :login, or, :cram_md5
     email: ENV['smtp_email'],
     user_name: ENV['smtp_user_name'],
