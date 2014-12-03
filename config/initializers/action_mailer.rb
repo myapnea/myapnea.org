efc6c233ad37e8b64feb62ed9f2e4d16f99cb634
Remo Mueller <remosm@gmail.com>
2014-12-03 15:44:57 -0500

    Only set domain if it is not blank

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -5,7 +5,7 @@ unless Rails.env.test?
     enable_starttls_auto: true,
     address: ENV['smtp_address'],
     port: ENV['smtp_port'].to_i,
-    domain: ENV['smtp_domain'],
+    domain: (ENV['smtp_domain'].blank? ? nil : ENV['smtp_domain']),
     authentication: ENV['smtp_authentication'].to_sym, # :plain, :login, or, :cram_md5
     email: ENV['smtp_email'],
     user_name: ENV['smtp_user_name'],
