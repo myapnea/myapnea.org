c733baed7b7e278f4bb60bdffbd322379ab88a83
pmanko <pmankowski@partners.org>
2014-12-08 14:21:27 -0500

    Forum fix

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -6,3 +6,12 @@ Forem.logged_in_layout = "main"
 Forem.avatar_user_method = :photo_url
 Forem.email_from_address = "#{ENV['website_name']} <#{ENV['smtp_email']}>"
 Forem.per_page = 20
+
+
+module Forem
+  class NilUser
+    def photo_url
+      "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest("NilUser")}?d=identicon"
+    end
+  end
+end
