677e18d3e293dfb4de79b7a3ffd3f8ecb73dc444
Remo Mueller <remosm@gmail.com>
2014-12-18 10:14:28 -0500

    Made cleantheme layout and application layout and javascript manifest files more similar

--- app/assets/javascripts/cleantheme.js
+++ app/assets/javascripts/cleantheme.js
@@ -20,7 +20,7 @@
 //= require d3.min
 //= require bootbox.min
 //= require typeahead
-// require uservoice
+//= require uservoice
 //= require google_analytics
 //= require bloodhound
 //= require bootstrap-tagsinput
