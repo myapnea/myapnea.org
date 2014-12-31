909351fb8150edb30d0bd9f0e71449ad1a2f3475
Remo Mueller <remosm@gmail.com>
2014-12-31 12:42:03 -0500

    Removed Forum controller

--- app/controllers/forem/forums_controller.rb
+++
@@ -1,9 +0,0 @@
-module Forem
-  class ForumsController < Forem::ApplicationController
-
-    def index
-      @categories = Forem::Category.all
-      redirect_to main_app.stealth_forums_path if params[:redesign] == '1'
-    end
-  end
-end
