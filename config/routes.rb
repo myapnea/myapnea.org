9ea35ff3f10c7578eb45421d90953b9470075a5a
Remo Mueller <remosm@gmail.com>
2015-01-16 14:21:36 -0500

    Started removing remaining `forem` gem code

--- config/routes.rb
+++ config/routes.rb
@@ -152,13 +152,6 @@ Rails.application.routes.draw do
 
   get 'forum', to: redirect("forums")
 
-  # # This line mounts Forem's routes at /forums by default.
-  # # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
-  # # If you would like to change where this extension is mounted, simply change the :at option to something different.
-
-  # # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
-  mount Forem::Engine, :at => '/old_forums'
-
 # # Authentication
 #   devise_for :user, skip: [:sessions, :passwords, :confirmations, :registrations]
 #   as :user do
