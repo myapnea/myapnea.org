5d55dcd36fa67f6f894e024fcf12d77c73104cd9
Piotr Mankowski <pmankowski@partners.org>
2015-02-25 08:01:53 -0500

    Updated migrate_survey method of AnswerMigration to use new answer_option map

--- lib/classes/answer_migration.rb
+++ lib/classes/answer_migration.rb
@@ -5,6 +5,7 @@
 =end
 
 class AnswerMigration
+  MAP_DIRECTORY = File.join(Rails.root, "lib", "data", "myapnea", "surveys", "answer_migration")
 
   def initialize(question_map)
     if question_map.is_a?(Array)
@@ -111,7 +112,7 @@ class AnswerMigration
 
   end
 
-  def migrate_survey(question_map, answer_option_map)
+  def migrate_survey(survey_slug, question_map=nil, answer_option_map=nil)
     # First pass:
     # 1. Go through the answers for a given question
     # 2. Find or create the answer session for the given user/survey combo
@@ -122,84 +123,62 @@ class AnswerMigration
 
     ## This should allow all historical values to be saved, without
 
-
-    SURVEY_LIST.each do |slug|
-      survey = Survey.find_by(slug: slug)
-      survey.all_questions_descendants.each do |question|
-        question.answer_templates.each do |answer_template|
-          option_list = answer_template.answer_options.map(&:text)
-          option_matcher = FuzzyMatch.new(option_list) if answer_template.answer_options.present?
-
-          matched_mapping = question_map.select {|mapping| (mapping["slug"] == question.slug and mapping["answer_template_name"] == answer_template.name) }.first
-          if matched_mapping
-            matched_question = Question.find(matched_mapping["id"].to_i)
-
-            matched_question.answers.each do |matched_answer|
-              matched_user = matched_answer.answer_session.user
-              #new_answer_session = matched_user.answer_sessions.find_or_create_by(survey_id: survey.id, encounter: "migrated")
-
-              #new_answer = Answer.new(question_id: question.id, answer_session_id: new_answer_session.id, state: "migrated")
+    question_map ||= YAML.load_file(File.join(MAP_DIRECTORY, "question_mappings.yml"))
+    answer_option_map ||= YAML.load_file(File.join(MAP_DIRECTORY, "answer_option_mappings.yml"))
 
 
+    survey = Survey.find_by(slug: survey_slug)
 
-              matched_answer.answer_values.each do |matched_answer_value|
-                #puts "#{matched_answer_value.show_value.blank? ? 0 : 1} | #{matched_answer_value.show_value}"
-                # Do not create empty answers!
-                unless matched_answer_value.show_value.blank?
+    survey.all_questions_descendants.each do |question|
+      question.answer_templates.each do |answer_template|
+        matched_mapping = question_map.select {|mapping| (mapping["slug"] == question.slug and mapping["answer_template_name"] == answer_template.name) }.first
 
-                  if answer_template.data_type == 'answer_option_id'
-                    # Target answer template is categorical
-                    if matched_answer_value.answer_template.data_type == "answer_option_id"
-                      # Matched answer value is categorical
-                      matched_option = option_matcher.find(matched_answer_value.show_value)
+        if matched_mapping
+          matched_question = Question.find(matched_mapping["id"].to_i)
 
-                      if matched_option
-                        sh = {question: question.slug, answer_template: answer_template.name, old: matched_answer_value.show_value, new: matched_option, list: option_list}
-                        matches_found << sh
-                        unique_matches << sh unless unique_matches.include?(sh)
-                      else
-                        sh = {question: question.slug, answer_template: answer_template.name, old: matched_answer_value.show_value, list: option_list}
-                        matches_not_found << sh
-                        unique_matches_not_found << sh unless unique_matches_not_found.include?(sh)
-                      end
+          matched_question.answers.each do |matched_answer|
+            matched_user = matched_answer.answer_session.user
+            new_answer_session = matched_user.answer_sessions.find_or_create_by(survey_id: survey.id, encounter: "migrated")
+
+            new_answer = Answer.new(question_id: question.id, answer_session_id: new_answer_session.id, state: "migrated")
 
 
 
-                      resolved_mappings << matched_mapping unless resolved_mappings.include?(matched_mapping)
-                    else
-                      # We need to map non-categorical ==> Categorical
-                      #puts "Categorization needed! #{matched_mapping}"
-                      unresolved_mappings << matched_mapping unless unresolved_mappings.include?(matched_mapping)
-                    end
+            matched_answer.answer_values.each do |matched_answer_value|
+              # Do not create empty answers!
+              unless matched_answer_value.show_value.blank?
+                matched_answer_template = matched_answer_value.answer_template
+
+                if answer_template.data_type == 'answer_option_id' and matched_answer_template.data_type == 'answer_option_id'
+                  # Target answer template is categorical and matched answer value is categorical
+                  matched_option_id = matched_answer_value.answer_option_id
+
+                  matched_answer_option_mapping = answer_option_map.select {|mapping| (mapping["old_option_id"] == matched_option_id and mapping["new_template_name"] == answer_template.name)}.first
+
+                  if matched_answer_option_mapping
+                    new_answer_option = answer_template.answer_options.find_by_value(matched_answer_option_mapping["new_option_value"])
+                    new_answer.answer_values.create(answer_template_id: answer_template.id, answer_option_id: new_answer_option.id)
+                    new_answer.save!
                   else
-                    # Target answer template is non-categorical
-                    if answer_template.data_type == matched_answer_value.answer_template.data_type
-                      # Match of types - just copy
-
-                      resolved_mappings << matched_mapping unless resolved_mappings.include?(matched_mapping)
-                    else
-                      # Type mismatch needs to be resolved
-                      unresolved_mappings << matched_mapping unless unresolved_mappings.include?(matched_mapping)
-                    end
+                    puts "Mapping not found! #{matched_option_id} #{answer_template.name}"
                   end
+
+                elsif answer_template.data_type == matched_answer_template.data_type
+                  # Match of types - just copy
+                  new_answer.answer_values.create(:answer_template_id => answer_template.id, answer_template.data_type => matched_answer_value.value)
+                  new_answer.save!
+                else
+                  puts "Unresolved match! #{matched_mapping["slug"]} | #{matched_mapping["answer_template_name"]}"
                 end
-                #new_answer_value = cloned_answer.answer_values.create(answer_template_id: matched_answer_value.answer_template_id, answer_option_id: matched_answer_value.answer_option_id, numeric_value: matched_answer_value.numeric_value, text_value: matched_answer_value.text_value, time_value: matched_answer_value.time_value)
-                #puts "#{new_answer_value.show_value} | #{matched_answer_value.answer_template.answer_options.map(&:text)} "
-
               end
-
-
-
-              #logger.debug cloned_answer.show_value
-
             end
-
-
           end
 
+
         end
+
       end
     end
   end
-  
+
 end
