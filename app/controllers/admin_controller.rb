76d5bc2bcc46e37bd606e3d9c7099bd8c03d64b9
Remo Mueller <remosm@gmail.com>
2015-03-09 15:23:49 -0400

    Fixed calculation of Avg # Completed surveys in survey statistics, addresses #458.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -22,7 +22,14 @@ class AdminController < ApplicationController
       { version: '4.2.0', release_date: Date.parse('2015-01-29'), next_release_date: Date.parse('2015-03-04') },
       { version: '4.1.0', release_date: Date.parse('2015-01-21'), next_release_date: Date.parse('2015-01-29') },
       { version: '4.0.0', release_date: Date.parse('2015-01-15'), next_release_date: Date.parse('2015-01-21') },
-      { version: 'Before 4.0.0', release_date: nil, next_release_date: Date.parse('2015-01-15') }
+      { version: '3.2.0', release_date: Date.parse('2015-01-08'), next_release_date: Date.parse('2015-01-15') },
+      { version: '3.1.0', release_date: Date.parse('2015-01-02'), next_release_date: Date.parse('2015-01-08') },
+      { version: '3.0.0', release_date: Date.parse('2014-12-16'), next_release_date: Date.parse('2015-01-02') },
+      { version: '2.1.0', release_date: Date.parse('2014-12-10'), next_release_date: Date.parse('2014-12-16') },
+      { version: '2.0.0', release_date: Date.parse('2014-11-14'), next_release_date: Date.parse('2014-12-10') },
+      { version: '1.1.0', release_date: Date.parse('2014-10-17'), next_release_date: Date.parse('2014-11-14') },
+      { version: '1.0.0', release_date: Date.parse('2014-10-03'), next_release_date: Date.parse('2014-10-17') },
+      { version: 'Before 1.0.0', release_date: nil, next_release_date: Date.parse('2014-10-03') }
     ]
   end
 
