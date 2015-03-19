8e11680b8ffa830e1aa9a731f153ae239350b9de
Piotr Mankowski <pmankowski@partners.org>
2015-03-19 11:10:06 -0400

    Added CDM queries

---
+++ db/views/common_data_model.sql
@@ -0,0 +1,54 @@
+﻿/* Demographics */
+
+/* Birth Date */
+select u.id, av.text_value from users u
+left join answer_sessions ans on ans.user_id = u.id
+join answers a on a.answer_session_id = ans.id
+join answer_values av on a.id = av.answer_id
+join questions q on a.question_id = q.id
+join answer_templates_questions qat on qat.question_id = q.id
+join answer_templates at on qat.answer_template_id = at.id
+where
+q.slug = 'date-of-birth' and at.name = 'birth_date';
+
+/* Sex */
+select u.id, ao.value, ao.text from users u
+left join answer_sessions ans on ans.user_id = u.id
+join answers a on a.answer_session_id = ans.id
+join answer_values av on a.id = av.answer_id
+join answer_options ao on av.answer_option_id = ao.id
+join questions q on a.question_id = q.id
+join answer_templates_questions qat on qat.question_id = q.id
+join answer_templates at on qat.answer_template_id = at.id
+where
+q.slug = 'sex' and at.name = 'sex';
+
+/* Hispanic Ethnicity */
+select u.id, ao.value, ao.text from users u
+left join answer_sessions ans on ans.user_id = u.id
+join answers a on a.answer_session_id = ans.id
+join answer_values av on a.id = av.answer_id
+join answer_options ao on av.answer_option_id = ao.id
+join questions q on a.question_id = q.id
+join answer_templates_questions qat on qat.question_id = q.id
+join answer_templates at on qat.answer_template_id = at.id
+where
+q.slug = 'ethnicity' and at.name = 'ethnicity';
+
+/* Race */
+select u.id, ao.value, ao.text from users u
+left join answer_sessions ans on ans.user_id = u.id
+join answers a on a.answer_session_id = ans.id
+join answer_values av on a.id = av.answer_id
+join answer_options ao on av.answer_option_id = ao.id
+join questions q on a.question_id = q.id
+join answer_templates_questions qat on qat.question_id = q.id
+join answer_templates at on qat.answer_template_id = at.id
+where
+q.slug = 'race' and at.name = 'race';
+
+/* Enrollments Table */
+select u.id, u.signed_consent_at from users u
+left join answer_sessions ans on ans.user_id = u.id
+
+
