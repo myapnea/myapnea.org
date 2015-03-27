2857840ef7b7a12a327d246fad3aa34c848a6547
Piotr Mankowski <pmankowski@partners.org>
2015-03-27 15:12:16 -0400

    Report documentation

--- app/models/surveys/report.rb
+++ app/models/surveys/report.rb
@@ -1,3 +1,47 @@
+=begin
+  Documentation
+
+  # A variable, encounter, and corresponding answer.
+  class FilterVariable
+
+  end
+
+  # The Main Class Filter object
+  # Contains groups by "user_id", along with numerous FilterVariables for the "user_id"
+  class Filter
+    attr_accessor :user_objects, :only_locked
+    def initialize(only_locked=true)
+       # `only_locked` is to require to only pull answers from answer_sessions that are locked
+      @only_locked = only_locked
+      @user_objects = []
+    end
+
+    # Starts building the data. Only data specified is pulled
+    def add_data!(survey_slug, variable_slug, encounter='baseline')
+    end
+
+    def where(variable_slug, encounter, conditional)
+      return self.with rows_removed_base_on_condition()
+    end
+
+    def count
+      return @user_objects.count
+    end
+
+  end
+
+
+
+
+@my_filter = Filter.new(false) # Use all submitted answers, not just locked answers
+@my_filter.add_data!('about-me', 'date-of-birth', 'baseline')
+@my_filter.add_data('additional-information-about-me','height','baseline')
+
+@my_filter.where('date-of-birth', 'baseline', "in?(45..50)").where('height', 'baseline', ">20").count
+
+=end
+
+
 class Report
 
   attr_reader :survey, :user, :encounter
