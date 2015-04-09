faff807dfd57bbb47dd21aa4d1ad66039cd5486e
Piotr Mankowski <pmankowski@partners.org>
2015-04-09 13:29:17 -0400

    Generated new survey yaml files from db with added answer option slugs for #487

--- app/models/surveys/survey.rb
+++ app/models/surveys/survey.rb
@@ -99,21 +99,28 @@ class Survey < ActiveRecord::Base
         at_hash = {}
         at_hash["name"] = at.name
         at_hash["data_type"] = at.data_type
-        at_hash["answer_options"] = at.answer_options.map do |ao|
-          ao_hash = {}
-          ao_hash["text"] = ao.text
-          ao_hash["hotkey"] = ao.hotkey
-          ao_hash["value"] = ao.value
-          ao_hash["display_class"] = ao.display_class
-          ao_hash["slug"] = "#{at.name}_#{ao.value}"
-          ao_hash
+        at_hash["text"] = at.text if at.text.present?
+        at_hash["unit"] = at.unit if at.unit.present?
+        at_hash["preprocess"] = at.preprocess if at.preprocess.present?
+        at_hash["allow_multiple"] = at.allow_multiple if at.allow_multiple
+        at_hash["target_answer_option"] = at.target_answer_option if at.target_answer_option.present?
+        if at.answer_options.present?
+          at_hash["answer_options"] = at.answer_options.map do |ao|
+            ao_hash = {}
+            ao_hash["slug"] = "#{at.name}_#{ao.value}".dasherize
+            ao_hash["text"] = ao.text
+            ao_hash["hotkey"] = ao.hotkey if ao.hotkey.present?
+            ao_hash["value"] = ao.value
+            ao_hash["display_class"] = ao.display_class if ao.display_class.present?
+            ao_hash
+          end
         end
         at_hash
       end
       q_hash
     end
 
-    File.open("/usr/local/htdocs/www_myapnea_org/lib/data/myapnea/surveys/generated/#{slug}.yml", 'w') {|f| f.write file_hash.to_yaml }
+    File.open(File.join(Rails.root, "/lib/data/myapnea/surveys/generated/#{slug}.yml"), 'w') {|f| f.write file_hash.to_yaml }
   end
 
   # Instance Methods
