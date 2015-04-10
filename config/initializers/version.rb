d62a6ffadeb220ffe4b39ffa0bc215a4ec9f08bb
Remo Mueller <remosm@gmail.com>
2015-04-10 13:32:27 -0400

    Version bump to 6.0.0.rc

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,7 +3,7 @@ module WwwMyapneaOrg
     MAJOR = 6
     MINOR = 0
     TINY = 0
-    BUILD = "beta1" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = "rc" # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
