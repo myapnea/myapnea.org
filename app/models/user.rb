67bb82852e2cefeeeb8c4e6557dcf101b5f718d2
Piotr Mankowski <pmankowski@partners.org>
2015-05-01 13:06:37 -0400

    Finished implementation of answer_text and answer_value for a given user, as specified in #596

--- app/models/user.rb
+++ app/models/user.rb
@@ -311,7 +311,7 @@ class User < ActiveRecord::Base
 
 
   # Reports
-  def report_value(params = {})
+  def answer_value(params = {})
     result_rows = Report.where({ user: self[:id] }.merge(params))
 
     answer_option_rows = result_rows.where(data_type: 'answer_option_id')
@@ -324,11 +324,48 @@ class User < ActiveRecord::Base
     end
   end
 
-  def report_text(params={})
-    rows = Report.where({ user: self[:id] }.merge(params))
+  def answer_text(params={})
+    result_rows = Report.where({ user: self[:id] }.merge(params))
 
-    if rows
+    # One answer value:
+    if result_rows.count == 1
+      result_row = result_rows.first
+
+      ## Answer option
+      ## Non-answer option
+      result_row.data_type == 'answer_option_id' ? result_row.answer_option_text : result_row.value
+
+    elsif result_rows.count > 1
+      if result_rows.where(allow_multiple: true).count == 0
+        # Two answer values, no multiple allowed
+        if result_rows.where(data_type: 'answer_option_id').pluck(:value).map(&:to_i).include? result_rows.pluck(:target_answer_option).compact.first
+          ## Non-answer option with target_answer_option selected
+          result_rows.where("target_answer_option is not null").pluck(:value).first
+        else
+          ## target_answer_option not selected
+          result_rows.where(data_type: 'answer_option_id').pluck(:answer_option_text).first
+        end
+      else
+        # Multiple Values Allowed:
+        ## Write-in with target_answer_option selected
+
+        all_ao_values = result_rows.where(data_type: 'answer_option_id').pluck(:value)
+        ignore_values = result_rows.pluck(:target_answer_option).compact
+
+
+
+        write_in_result_rows = result_rows.where("target_answer_option is not null").where(target_answer_option: all_ao_values)
+        ao_result_rows = result_rows.where(data_type: 'answer_option_id')
+        ao_result_rows = ao_result_rows.where.not(value: ignore_values.map(&:to_s)) if ignore_values
+
+        ## Just answer option answer
+        write_in_result_rows.pluck(:value) + ao_result_rows.pluck(:answer_option_text)
+      end
     end
+
+
+
+
 
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,23 +20,46 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-  test "#report_value" do
+  test "#answer_value" do
     # For questions with answer option templates, return the value of the answer option
-    assert_equal "2", users(:social).report_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'countries'})
-    assert_equal "2", users(:social).report_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline'})
-    assert_equal "3", users(:participant).report_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'countries'})
-    assert_equal "3", users(:participant).report_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline'})
+    assert_equal "2", users(:social).answer_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'countries'})
+    assert_equal "2", users(:social).answer_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline'})
+    assert_equal "3", users(:participant).answer_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'countries'})
+    assert_equal "3", users(:participant).answer_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline'})
 
     # For questions with no answer option template, return their value
-    assert_equal "Easter Island", users(:participant).report_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'spec_countries'})
+    assert_equal "Easter Island", users(:participant).answer_value({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'spec_countries'})
 
     # Return array of values if multiple values are allowed (checkbox)
-    multiple_values = users(:has_completed_survey).report_value({survey_slug: 'new-survey', question_slug: 'race-selection', encounter: 'baseline', answer_template_name: 'fixture_race'})
+    multiple_values = users(:has_completed_survey).answer_value({survey_slug: 'new-survey', question_slug: 'race-selection', encounter: 'baseline', answer_template_name: 'fixture_race'})
     assert multiple_values.kind_of?(Array)
     assert_includes multiple_values, "2"
     assert_includes multiple_values, "5"
 
     # Return nil if answer does not exist
-    assert_nil users(:has_incomplete_survey).report_value({survey_slug: '', question_slug: 'country', encounter: 'baseline'})
+    assert_nil users(:has_incomplete_survey).answer_value({survey_slug: '', question_slug: 'country', encounter: 'baseline'})
+  end
+
+  test "#answer_text" do
+    # For questions with only an answer option template, return the text of the answer option
+    assert_equal "Poland", users(:social).answer_text({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'countries'})
+    assert_equal "Poland", users(:social).answer_text({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline'})
+    assert_equal "Other", users(:participant).answer_text({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'countries'})
+
+    # For questions with an "other" option selected, return the text value of the write-in
+    assert_equal "Easter Island", users(:participant).answer_text({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline'})
+
+    # For questions with no answer option template, return their value
+    assert_equal "Easter Island", users(:participant).answer_text({survey_slug: 'for-reports', question_slug: 'country', encounter: 'baseline', answer_template_name: 'spec_countries'})
+
+    # Return array of texts if multiple values are allowed (checkbox)
+    multiple_values = users(:has_completed_survey).answer_text({survey_slug: 'new-survey', question_slug: 'race-selection', encounter: 'baseline', answer_template_name: 'fixture_race'})
+    assert multiple_values.kind_of?(Array)
+    assert_includes multiple_values, "Bith"
+    assert_includes multiple_values, "Rodian"
+
+    # Return nil if answer does not exist
+    assert_nil users(:has_incomplete_survey).answer_text({survey_slug: '', question_slug: 'country', encounter: 'baseline'})
+
   end
 end
