ac72756a997eee13d9121d6cc4ea63e02e333152
Piotr Mankowski <pmankowski@partners.org>
2015-05-12 11:44:15 -0400

    Fixed pgsql 8.4 compatibility issues for #647

--- app/models/research_topics/research_topic.rb
+++ app/models/research_topics/research_topic.rb
@@ -23,14 +23,14 @@ class ResearchTopic < ActiveRecord::Base
   # Named Scopes
   scope :approved, lambda { current.joins(:topic).where(topics: {status: 'approved'})}
   scope :pending_review, lambda { current.joins(:topic).where(topics: {status: 'pending_review'})}
-  scope :most_voted, lambda { current.select("research_topics.*, count(votes.id) as vote_count").joins("left outer join votes on votes.research_topic_id = research_topics.id and votes.deleted = 'f'").group("research_topics.id").order("vote_count desc") }
-  scope :least_voted, lambda { current.select("research_topics.*, count(votes.id) as vote_count").joins("left outer join votes on votes.research_topic_id = research_topics.id and votes.deleted = 'f'").group("research_topics.id").order("vote_count asc") }
-  scope :most_discussed, lambda { current.select("research_topics.*, count(posts.id) as post_count").joins(topic: :posts).group("research_topics.id").order("post_count desc") }
+  scope :most_voted, lambda { current.select("research_topics.*, count(votes.id) as vote_count").joins("left outer join votes on votes.research_topic_id = research_topics.id and votes.deleted = 'f'").group(group_columns).order("vote_count desc") }
+  scope :least_voted, lambda { current.select("research_topics.*, count(votes.id) as vote_count").joins("left outer join votes on votes.research_topic_id = research_topics.id and votes.deleted = 'f'").group(group_columns).order("vote_count asc") }
+  scope :most_discussed, lambda { current.select("research_topics.*, count(posts.id) as post_count").joins(topic: :posts).group(group_columns).order("post_count desc") }
   scope :newest, lambda { current.order("research_topics.created_at desc") }
 
   # Class methods
   def self.popular(vote_threshold = nil)
-    query = current.select("research_topics.*, (sum(votes.rating)::float/count(votes.rating)::float) as endorsement").joins(:votes).group("research_topics.id").order("endorsement desc")
+    query = current.select("research_topics.*, (sum(votes.rating)::float/count(votes.rating)::float) as endorsement").joins(:votes).group(group_columns).order("endorsement desc")
     query = query.having("count(votes.rating) > ?", vote_threshold) if vote_threshold.present?
     query
   end
@@ -40,7 +40,7 @@ class ResearchTopic < ActiveRecord::Base
     approved
       .select("research_topics.*, count(votes.id) as vote_count")
       .joins("left outer join votes on votes.research_topic_id = research_topics.id and votes.deleted = 'f'")
-      .group("research_topics.id").having("sum(case when votes.user_id = ? then 1 else 0 end) = 0", user.id)
+      .group(group_columns).having("sum(case when votes.user_id = ? then 1 else 0 end) = 0", user.id)
       .order("vote_count asc")
       .first
   end
@@ -77,6 +77,10 @@ class ResearchTopic < ActiveRecord::Base
 
 
   private
+
+  def self.group_columns
+    column_names.map{|cn| "research_topics.#{cn}"}.join(", ")
+  end
 
   def create_associated_topic
 
