26e52eb11e1c2542ea4fcba325c356d3217230a7
Kyle Rand <krand7@gmail.com>
2015-05-29 17:48:58 -0400

    Rank the research visible to public, for #709

--- test/controllers/research_topics_controller_test.rb
+++ test/controllers/research_topics_controller_test.rb
@@ -7,9 +7,15 @@ class ResearchTopicsControllerTest < ActionController::TestCase
     @novice_user = users(:user_4)
     @experienced_user = users(:user_1)
 
+    @rt = research_topics(:rt1)
   end
 
   # Index
+
+  test "should get index for logged out user" do
+    get :index
+    assert_response :success
+  end
 
   test "should get index for experienced user" do
     # Displays normal index
@@ -32,116 +38,121 @@ class ResearchTopicsControllerTest < ActionController::TestCase
   end
 
 
-  # Intro
+  # Intro : page explaining rank the research and allowing user to press "get started"
+  test "should get intro and redirect for logged out user" do
+    get :intro
+    assert_redirected_to research_topics_path
+  end
 
   test "should get intro for no_votes user" do
-    # page explaining rank the research and allowing user to press "get started"
     login(@no_votes_user)
-
     get :intro
-
     assert_response :success
-
   end
 
 
-  # First Topics
-  test "should get first topics for novice user" do
-    ResearchTopic.load_seeds
-    login(@novice_user)
-
+  # First Topics - first 10 topics
+  test "should get first topics and redirect for logged out user" do
     get :first_topics
+    assert_redirected_to research_topics_path
+  end
 
+  test "should get first topics for novice user" do
+    login(@novice_user)
+    ResearchTopic.load_seeds
+    get :first_topics
     assert_response :success
-
   end
 
   test "should get first topics for no_votes user that read the intro" do
-    ResearchTopic.load_seeds
     login(@no_votes_user)
-
+    ResearchTopic.load_seeds
     get :first_topics, read_intro: 1
-
     assert_response :success
   end
 
   test "should redirect no_votes user to intro if they haven't read the intro" do
     login(@no_votes_user)
-
     get :first_topics
-
     assert_redirected_to intro_research_topics_path
   end
 
-  # Not sure this is the best course of action:
   test "should redirect from first topics to index for experienced users" do
     login(@experienced_user)
-
     get :first_topics
-
     assert_redirected_to research_topics_path
-
   end
 
-  # Newest
+
+  # Newest - index of newest topic pages
+  test "should get newest for logged out user" do
+    get :newest
+    assert_response :success
+  end
+
   test "should get newest for experienced user" do
     login(@experienced_user)
-
     get :newest
-
     assert_response :success
   end
 
   test "should redirect from newest to intro action for no_votes user" do
     login(@no_votes_user)
-
     get :newest
-
     assert_redirected_to intro_research_topics_path
   end
 
   test "should redirect from newest to first_topics action for novice_user" do
     login(@novice_user)
-
     get :newest
-
     assert_redirected_to first_topics_research_topics_path
   end
 
+
   # Most Discussed
+  test "should get most discussed for logged out user" do
+    get :most_discussed
+    assert_response :success
+  end
+
   test "should get most_discussed for experienced user" do
     login(@experienced_user)
-
     get :most_discussed
-
     assert_response :success
   end
 
   test "should redirect from most discussed to intro action for no_votes user" do
     login(@no_votes_user)
-
     get :most_discussed
-
     assert_redirected_to intro_research_topics_path
   end
 
   test "should redirect from most discussed to first_topics action for novice_user" do
     login(@novice_user)
-
     get :most_discussed
-
     assert_redirected_to first_topics_research_topics_path
   end
 
 
-  # Creation
+  # Show pages
+  test "should get show page as experienced user" do
+    login(@experienced_user)
+    get :show, id: @rt
+    assert_response :success
+  end
 
+  test "should get show page as logged out user" do
+    get :show, id: @rt
+    assert_response :success
+  end
+
+
+  # Creation
   test "should create a research topic as experienced user" do
     login(@experienced_user)
     assert_difference('ResearchTopic.count') do
       post :create, research_topic: { text: 'Some new research topic', description: 'Why I think this is important'}
     end
-
     assert_redirected_to research_topic_path(ResearchTopic.last)
   end
 
@@ -151,13 +162,10 @@ class ResearchTopicsControllerTest < ActionController::TestCase
     assert_no_difference('ResearchTopic.count') do
       post :create, research_topic: { text: 'Some new research topic', description: 'Why I think this is important'}
     end
-
-
   end
 
   test "should not create research topic as no votes user" do
     login(@no_votes_user)
-
     assert_no_difference('ResearchTopic.count') do
       post :create, research_topic: { text: 'Some new research topic', description: 'Why I think this is important'}
     end
@@ -169,6 +177,7 @@ class ResearchTopicsControllerTest < ActionController::TestCase
     end
 
   end
+
 
   # My Topics
   test "should get my topics for experienced user" do
@@ -191,24 +200,25 @@ class ResearchTopicsControllerTest < ActionController::TestCase
     assert_redirected_to first_topics_research_topics_path
   end
 
+  test "should get my topics and redirect to index for logged out user" do
+    get :my_research_topics
+    assert_redirected_to research_topics_path
+  end
+
+
   # Endorsement
   # Test w/ and w/o comment
-
   test "should endorse any approved research topic as experienced user" do
     @request.env['HTTP_REFERER'] = 'http://localhost:3000/sessions/new'
 
     login(@experienced_user)
-
     assert_nil research_topics(:rt2).endorsement
 
     assert_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: research_topics(:rt2).id, "endorse_#{research_topics(:rt2).id}" => 1, format: 'js'
     end
-
     assert_equal 1.0, research_topics(:rt2).endorsement
-
     assert_not_nil assigns(:research_topic)
-
     assert_response :success
   end
 
@@ -221,22 +231,19 @@ class ResearchTopicsControllerTest < ActionController::TestCase
     assert_difference "Post.count" do
       xhr :post, :vote, research_topic_id: research_topics(:rt2).id, "endorse_#{research_topics(:rt2).id}" => 1, "comment_#{research_topics(:rt2).id}" => comment, format: 'js'
     end
-
     assert_equal comment, research_topics(:rt2).topic.posts.last.description
-
   end
 
   test "should vote for only the seeded research topics as a no_votes user" do
     @request.env['HTTP_REFERER'] = 'http://localhost:3000/sessions/new'
+
     login(@no_votes_user)
     ResearchTopic.load_seeds
     rt = ResearchTopic.where(category: "seeded").first
 
-
     assert_no_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: research_topics(:rt2).id, "endorse_#{research_topics(:rt2).id}" => 1, format: 'js'
     end
-
     assert_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: rt.id, "endorse_#{rt.id}" => 1, format: 'js'
     end
@@ -246,37 +253,32 @@ class ResearchTopicsControllerTest < ActionController::TestCase
 
   test "should vote for only the seeded research topics as a novice user" do
     @request.env['HTTP_REFERER'] = 'http://localhost:3000/sessions/new'
+
     login(@novice_user)
     ResearchTopic.load_seeds
     rt = ResearchTopic.where(category: "seeded").first
 
-
     assert_no_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: research_topics(:rt2).id, "endorse_#{research_topics(:rt2).id}" => 1, format: 'js'
     end
-
     assert_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: rt.id, "endorse_#{rt.id}" => 1, format: 'js'
     end
-
   end
 
 
   # Opposition
   test "should oppose any approved research topic as experienced user" do
     @request.env['HTTP_REFERER'] = 'http://localhost:3000/sessions/new'
+
     login(@experienced_user)
 
     assert_nil research_topics(:rt2).endorsement
-
     assert_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: research_topics(:rt2).id, "endorse_#{research_topics(:rt2).id}" => 0, format: 'js'
     end
-
     assert_equal 0.0, research_topics(:rt2).endorsement
-
     assert_not_nil assigns(:research_topic)
-
     assert_response :success
   end
 
@@ -285,15 +287,10 @@ class ResearchTopicsControllerTest < ActionController::TestCase
     @request.env['HTTP_REFERER'] = 'http://localhost:3000/sessions/new'
     login(@experienced_user)
 
-
     assert_no_difference "Vote.count" do
       xhr :post, :vote, research_topic_id: research_topics(:rt2).id, format: 'js'
     end
-
-
   end
-
-
 
 
 end
