484d38d0d37a1c4a8a06a7198db551ce49bab197
Kyle Rand <krand7@gmail.com>
2015-07-07 13:32:11 -0400

    Version bump to 7.3.2.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,8 +2,8 @@ module WwwMyapneaOrg
   module VERSION #:nodoc:
     MAJOR = 7
     MINOR = 3
-    TINY = 1
-    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
+    TINY = 2
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
