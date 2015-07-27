ae24f1186dfc11d9835a8ef958a5f85c06266541
Remo Mueller <remosm@gmail.com>
2015-07-27 11:13:12 -0400

    Version bump to 8.0.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,9 +1,9 @@
 module WwwMyapneaOrg
   module VERSION #:nodoc:
-    MAJOR = 7
-    MINOR = 3
-    TINY = 2
-    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
+    MAJOR = 8
+    MINOR = 0
+    TINY = 0
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
