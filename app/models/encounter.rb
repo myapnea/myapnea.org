4beda376fa39e1eec7e1b6ebdd95024458b056b9
Remo Mueller <remosm@gmail.com>
2015-08-13 12:57:23 -0400

    Encounters must have zero or positive "launch_days_after_sign_up", addresses #794.

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -7,6 +7,7 @@ class Encounter < ActiveRecord::Base
   validates_presence_of :name, :slug, :launch_days_after_sign_up
   validates_uniqueness_of :slug, scope: [ :survey_id, :deleted ]
   validates_format_of :slug, with: /\A(?!\Anew\Z)[a-z0-9][a-z0-9\-]*\Z/
+  validates_numericality_of :launch_days_after_sign_up, greater_than_or_equal_to: 0, less_than_or_equal_to: 5000
 
   # Model Relationships
   belongs_to :user
