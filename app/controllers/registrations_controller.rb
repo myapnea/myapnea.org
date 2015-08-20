7b78ec63f2d0c940b03f52b46806695b86a1a0a0
Remo Mueller <remosm@gmail.com>
2015-08-20 17:21:47 -0400

    Removed unused method in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -23,8 +23,4 @@ class RegistrationsController < Devise::RegistrationsController
 
   end
 
-  def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :year_of_birth, :zip_code, :email, :password, :password_confirmation, :current_password)
-  end
-
 end
