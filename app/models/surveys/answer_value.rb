0914c686dd4e4eccfddaef586170af21fe9079f1
Remo Mueller <remosm@gmail.com>
2015-10-19 15:09:27 -0400

    Dates are now correctly formatted for SAS export, fixes #850.

--- app/models/surveys/answer_value.rb
+++ app/models/surveys/answer_value.rb
@@ -3,6 +3,8 @@ class AnswerValue < ActiveRecord::Base
   belongs_to :answer_option
   belongs_to :answer_template
 
+  include DateAndTimeParser
+
   def value
     self[answer_template.data_type]
   end
@@ -23,7 +25,12 @@ class AnswerValue < ActiveRecord::Base
     if answer_template.data_type == 'answer_option_id'
       answer_option ? answer_option.value : nil
     else
-      value
+      if answer_template.template_name == 'date'
+        date = parse_date(text_value)
+        date ? date.strftime('%Y-%m-%d') : nil
+      else
+        value
+      end
     end
   end
 end
