828d0f945981aab1412b0742c8400154412e5cfb
Kyle Rand <krand7@gmail.com>
2015-12-07 16:37:03 -0500

    Added industry sponsored flag to clinical trial whitelist, for #886

--- app/controllers/admin/clinical_trials_controller.rb
+++ app/controllers/admin/clinical_trials_controller.rb
@@ -73,6 +73,6 @@ class Admin::ClinicalTrialsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_clinical_trial_params
-      params.require(:admin_clinical_trial).permit(:title, :overview, :description, :eligibility, :phone, :email, :link, :displayed)
+      params.require(:admin_clinical_trial).permit(:title, :overview, :description, :eligibility, :phone, :email, :link, :displayed, :industry_sponsored)
     end
 end
