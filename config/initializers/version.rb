0ceabd0630001ba4d3cc2f999ada4ac6aafc9fad
Remo Mueller <remosm@gmail.com>
2016-03-15 13:11:04 -0400

    Version bump to 10.2.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,9 +3,9 @@
 module WwwMyapneaOrg
   module VERSION #:nodoc:
     MAJOR = 10
-    MINOR = 1
-    TINY = 1
-    BUILD = nil # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    MINOR = 2
+    TINY = 0
+    BUILD = 'pre' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
