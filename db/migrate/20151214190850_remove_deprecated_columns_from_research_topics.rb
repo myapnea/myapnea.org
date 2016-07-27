20ebc7a94da40371aec4a20e02cc24a336c3592c
Remo Mueller <remosm@gmail.com>
2016-07-27 17:21:53 -0400

    Improved reversibility of migrations

--- db/migrate/20151214190850_remove_deprecated_columns_from_research_topics.rb
+++ db/migrate/20151214190850_remove_deprecated_columns_from_research_topics.rb
@@ -1,7 +1,7 @@
 class RemoveDeprecatedColumnsFromResearchTopics < ActiveRecord::Migration[4.2]
   def change
-    remove_column :research_topics, :text_deprecated
-    remove_column :research_topics, :description_deprecated
-    remove_column :research_topics, :state_deprecated
+    remove_column :research_topics, :text_deprecated, :string
+    remove_column :research_topics, :description_deprecated, :text
+    remove_column :research_topics, :state_deprecated, :string, null: false, default: 'under_review'
   end
 end
