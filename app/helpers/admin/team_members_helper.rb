d348a04e1c02587f5e8cb8655fb9c60592e81391
Remo Mueller <remosm@gmail.com>
2016-07-28 15:03:40 -0400

    Removed unused files

--- app/helpers/admin/team_members_helper.rb
+++ app/helpers/admin/team_members_helper.rb
@@ -1,5 +1,4 @@
 module Admin::TeamMembersHelper
-
   def team_member_photo_url(team_member)
     if team_member.photo.present?
       photo_admin_team_member_path(team_member)
@@ -7,5 +6,4 @@ module Admin::TeamMembersHelper
       'default-user.jpg'
     end
   end
-
 end
