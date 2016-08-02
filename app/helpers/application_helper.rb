8802ad5d4e3637a82e6b2b70225901fbb312ea66
Remo Mueller <remosm@gmail.com>
2016-08-02 17:16:50 -0400

    Removed unused methods and fixtures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,23 +11,6 @@ module ApplicationHelper
     YAML.load_file(Rails.root.join('lib', 'data', 'content', "#{name}.yml"))[name]
   end
 
-  def get_tracked_link(link_id)
-    link_with_tracking = "tracking_#{link_id}"
-
-    default_links = {
-      check_risk: sleep_apnea_risk_assessment_path,
-      surveys: surveys_path,
-      facebook: "https://www.facebook.com/sharer/sharer.php?u=www.myapnea.org",
-      join_from_landing: new_user_registration_path,
-      join_from_sidebar: new_user_registration_path,
-      join_from_surveys: new_user_registration_path,
-      join_from_research: new_user_registration_path,
-      join_from_learn: new_user_registration_path
-    }
-
-    ENV[link_with_tracking].present? ? ENV[link_with_tracking] : default_links[link_id]
-  end
-
   def th_sort_field_rev(order, sort_field, display_name, extra_class: '')
     sort_params = params.permit(:search)
     sort_field_order = (order == "#{sort_field} desc" || order == "#{sort_field} desc nulls last") ? sort_field : "#{sort_field} desc"
