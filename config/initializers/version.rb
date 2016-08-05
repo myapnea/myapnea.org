00fbcf95dfcb2ce402ebf5c9c8e2b3deb3081d23
Remo Mueller <remosm@gmail.com>
2016-08-05 15:23:27 -0400

    Version bump to 14.0.0.beta4

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module WwwMyapneaOrg
     MAJOR = 14
     MINOR = 0
     TINY = 0
-    BUILD = 'beta3' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = 'beta4' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
