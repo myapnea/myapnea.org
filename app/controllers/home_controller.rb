7907394a69afb1ff17de4ca1dc2cca282074aef2
Remo Mueller <remosm@gmail.com>
2017-04-10 16:55:07 -0400

    Minor fixes to tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,7 +16,7 @@ class HomeController < ApplicationController
 
       current_user.is_only_researcher? ? load_academic_dashboard_resources : load_member_dashboard_resources
     else
-      render 'home/landing'
+      render 'external/landing'
     end
   end
 
--- test/controllers/external_controller_test.rb
+++ test/controllers/external_controller_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 
 # Test for publicly available pages.
 class ExternalControllerTest < ActionController::TestCase
+  setup do
+    @regular_user = users(:user_1)
+  end
+
   test 'should get contact' do
     get :contact
     assert_response :success
@@ -16,6 +20,17 @@ class ExternalControllerTest < ActionController::TestCase
 
   test 'should get voting' do
     get :voting
+    assert_response :success
+  end
+
+  test 'should get landing' do
+    get :landing
+    assert_response :success
+  end
+
+  test 'should get landing for regular user' do
+    login(@regular_user)
+    get :landing
     assert_response :success
   end
 end
