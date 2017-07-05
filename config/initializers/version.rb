18800fd016fa598cc24e620065a068200c204d24
Remo Mueller <remosm@gmail.com>
2017-07-05 15:37:12 -0400

    Prefer double quotes over single quotes

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,8 +5,8 @@ module WwwMyapneaOrg
     MAJOR = 16
     MINOR = 0
     TINY = 0
-    BUILD = 'beta1' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = "beta1" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
 end
