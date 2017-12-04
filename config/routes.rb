a05c6894f41256872c5763a88ea81e1c2150b7d8
Remo Mueller <remosm@gmail.com>
2017-12-04 12:48:53 -0500

    Minor updates to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'external#landing'
+  root "external#landing"
 
   namespace :async do
     namespace :blog do
@@ -48,13 +48,13 @@ Rails.application.routes.draw do
 
   scope module: :blog do
     get :blog
-    get 'blog/category/:category', action: 'blog', as: :blog_category
-    get 'blog/author/:author', action: 'blog', as: :blog_author
-    get 'blog/:slug', action: 'show'
-    get 'blog/:year/:month/:slug', action: 'show', as: :blog_post
+    get "blog/category/:category", action: "blog", as: :blog_category
+    get "blog/author/:author", action: "blog", as: :blog_author
+    get "blog/:slug", action: "show"
+    get "blog/:year/:month/:slug", action: "show", as: :blog_post
   end
 
-  resources :broadcasts, path: 'editor/blog'
+  resources :broadcasts, path: "editor/blog"
 
   resources :broadcast_comments do
     collection do
@@ -70,20 +70,21 @@ Rails.application.routes.draw do
       post :upload, action: :create_multiple
     end
   end
-  get '/image/:id' => 'images#download', as: 'download_image'
+  get "/image/:id" => "images#download", as: "download_image"
 
   scope module: :external do
     post :preview
     get :contact
     get :landing
+    get :sitemap_xml, path: "sitemap.xml.gz"
     get :voting
     get :version
   end
 
   scope module: :blank do
-    get :menu, path: 'question/menu'
-    get :yoga, path: 'mindfulness/yoga-usefulness-scale(/:id)'
-    get :question, path: 'question(/:id)'
+    get :menu, path: "question/menu"
+    get :yoga, path: "mindfulness/yoga-usefulness-scale(/:id)"
+    get :question, path: "question(/:id)"
     get :sky
     get :sunset
     get :night
@@ -100,17 +101,17 @@ Rails.application.routes.draw do
 
   scope module: :internal do
     get :dashboard
-    get :activity, path: 'dashboard/activity'
-    get :research, path: 'dashboard/research'
-    get :reports, path: 'dashboard/reports'
+    get :activity, path: "dashboard/activity"
+    get :research, path: "dashboard/research"
+    get :reports, path: "dashboard/reports"
     get :timeline
-    get :yoga_consent, path: 'yoga/consent'
-    get :research, path: 'dashboard/research'
+    get :yoga_consent, path: "yoga/consent"
+    get :research, path: "dashboard/research"
     get :settings
-    get :settings_account, path: 'settings/account'
-    get :settings_consents, path: 'settings/consents'
-    get :settings_emails, path: 'settings/emails'
-    get :settings_profile, path: 'settings/profile'
+    get :settings_account, path: "settings/account"
+    get :settings_consents, path: "settings/consents"
+    get :settings_emails, path: "settings/emails"
+    get :settings_profile, path: "settings/profile"
   end
 
   resources :projects do
@@ -123,18 +124,18 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # Static Pages
-  scope module: 'static' do
+  scope module: "static" do
     get :about
     get :team
     get :partners
   end
 
-  get 'members', to: 'members#index', as: :members
-  get 'members/:forum_name', to: 'members#show', as: :member
-  get 'members/:forum_name/posts', to: 'members#posts', as: :member_posts
-  get 'members/:forum_name/badges', to: 'members#badges', as: :member_badges
+  get "members", to: "members#index", as: :members
+  get "members/:forum_name", to: "members#show", as: :member
+  get "members/:forum_name/posts", to: "members#posts", as: :member_posts
+  get "members/:forum_name/badges", to: "members#badges", as: :member_badges
   scope module: :members do
-    get '/photo/:forum_name', action: 'photo', as: :photo_member
+    get "/photo/:forum_name", action: "photo", as: :photo_member
   end
 
   resources :notifications do
@@ -144,7 +145,7 @@ Rails.application.routes.draw do
   end
 
   scope module: :search do
-    get :search, action: 'index', as: :search
+    get :search, action: "index", as: :search
   end
 
   # Surveys
@@ -154,20 +155,20 @@ Rails.application.routes.draw do
       post :submit
     end
     member do
-      get '(/:encounter)/report', action: 'report', as: :report
-      get '(/:encounter)', action: 'show', as: :show
-      get '(/:encounter)/report-detail', action: 'report_detail', as: :report_detail
-      get ':encounter/accept-update-first(/:child_id)', action: :accept_update_first, as: :accept_update_first
+      get "(/:encounter)/report", action: "report", as: :report
+      get "(/:encounter)", action: "show", as: :show
+      get "(/:encounter)/report-detail", action: "report_detail", as: :report_detail
+      get ":encounter/accept-update-first(/:child_id)", action: :accept_update_first, as: :accept_update_first
     end
   end
 
   # Admin Section
-  get 'admin' => 'admin#dashboard'
+  get "admin" => "admin#dashboard"
 
   devise_for :users,
-             controllers: { registrations: 'registrations', sessions: 'sessions' },
-             path_names: { sign_up: 'join', sign_in: 'login' },
-             path: ''
+             controllers: { registrations: "registrations", sessions: "sessions" },
+             path_names: { sign_up: "join", sign_in: "login" },
+             path: ""
 
   resources :users do
     collection do
@@ -175,10 +176,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :topics, path: 'forum' do
+  resources :topics, path: "forum" do
     member do
-      get '/edit', action: :edit, as: :edit
-      get '/:page', action: :show, as: :page
+      get "/edit", action: :edit, as: :edit
+      get "/:page", action: :show, as: :page
     end
   end
 
@@ -192,8 +193,6 @@ Rails.application.routes.draw do
   end
 
   # TODO: Remove redirect after November 1, 2017
-  get '/providers(/:slug)', to: redirect('landing')
+  get "/providers(/:slug)", to: redirect("landing")
   # END TODO
-
-  get 'sitemap.xml.gz' => 'external#sitemap'
 end
