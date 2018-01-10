67e044c787057d0c919b04d0f818de8a7c4cebe4
Remo Mueller <remosm@gmail.com>
2018-01-10 13:13:02 -0500

    Fixed tests for internal pages

--- test/controllers/internal_controller_test.rb
+++ test/controllers/internal_controller_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Test to assure that interal pages load properly for users.
 class InternalControllerTest < ActionDispatch::IntegrationTest
@@ -8,57 +8,45 @@ class InternalControllerTest < ActionDispatch::IntegrationTest
     @regular_user = users(:user_1)
   end
 
-  test 'should get dashboard' do
-    sign_in_as(@regular_user, 'password')
-    get dashboard_path
-    assert_redirected_to activity_path
-  end
-
-  test 'should get dashboard activity' do
-    sign_in_as(@regular_user, 'password')
-    get activity_path
+  test "should get dashboard" do
+    login(@regular_user)
+    get dashboard_url
     assert_response :success
   end
 
-  test 'should get dashboard research' do
-    sign_in_as(@regular_user, 'password')
-    get research_path
+  test "should get dashboard reports" do
+    login(@regular_user)
+    get reports_url
     assert_response :success
   end
 
-  test 'should get dashboard reports' do
-    sign_in_as(@regular_user, 'password')
-    get reports_path
+  test "should get timeline" do
+    login(@regular_user)
+    get timeline_url
     assert_response :success
   end
 
-  test 'should get timeline' do
-    sign_in_as(@regular_user, 'password')
-    get timeline_path
+  test "should get research" do
+    login(@regular_user)
+    get research_url
     assert_response :success
   end
 
-  test 'should get research' do
-    sign_in_as(@regular_user, 'password')
-    get research_path
+  test "should get settings" do
+    login(@regular_user)
+    get settings_url
+    assert_redirected_to settings_profile_url
+  end
+
+  test "should get settings account" do
+    login(@regular_user)
+    get settings_account_url
     assert_response :success
   end
 
-  test 'should get settings' do
-    sign_in_as(@regular_user, 'password')
-    get settings_path
-    assert_redirected_to settings_profile_path
-  end
-
-  test 'should get settings account' do
-    sign_in_as(@regular_user, 'password')
-    get settings_account_path
-    assert_response :success
-  end
-
-  test 'should get settings consents' do
-    sign_in_as(@regular_user, 'password')
-    get settings_consents_path
+  test "should get settings consents" do
+    login(@regular_user)
+    get settings_consents_url
     assert_response :success
   end
 end
