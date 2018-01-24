9df1b9332cb35da938065e276d33581781330c66
Remo Mueller <remosm@gmail.com>
2018-01-24 12:49:24 -0500

    Allow bolding of questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     content_tag(:i, "", class: "fa #{checked ? "fa-check-square-o" : "fa-square-o"}")
   end
 
+  def simple_bold(text)
+    text.to_s.gsub(/\*\*(.*?)\*\*/, "<strong>\\1</strong>").html_safe
+  end
+
   # TODO: Remove references to page_content
   def page_content(name)
     YAML.load_file(Rails.root.join('lib', 'data', 'content', "#{name}.yml"))[name]
