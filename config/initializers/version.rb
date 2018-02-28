9dc5d55f8f4528ae721691690e75db80f49d1f71
Remo Mueller <remosm@gmail.com>
2018-02-28 18:55:10 -0500

    Version bump to 17.0.0.beta1

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module Myapnea
     MAJOR = 17
     MINOR = 0
     TINY = 0
-    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
+    BUILD = "beta1" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
