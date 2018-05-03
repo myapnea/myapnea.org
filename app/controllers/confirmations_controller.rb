cc427af904f87761e4e05e14ed4e6dccb325a980
Remo Mueller <remosm@gmail.com>
2018-05-03 15:34:18 -0400

    Made confirmation step easier

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,4 +3,9 @@
 # Override for devise confirmations controller.
 class ConfirmationsController < Devise::ConfirmationsController
   layout "layouts/full_page"
+
+  def after_confirmation_path_for(resource_name, resource)
+    sign_in(resource)
+    super
+  end
 end
