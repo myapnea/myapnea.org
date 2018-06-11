6aff26a05d62640e143d5424e379f260c136df55
Remo Mueller <remosm@gmail.com>
2018-06-11 15:39:27 -0400

    Refactored topics controller test

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -6,7 +6,7 @@ require "test_helper"
 class TopicsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @topic = topics(:one)
-    @regular_user = users(:user_1)
+    @regular = users(:regular)
   end
 
   def topic_params
@@ -19,19 +19,17 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should subscribe to notifications" do
-    login(users(:regular))
+    login(@regular)
     post subscription_topic_url(@topic, format: "js"), params: { notify: "1" }
-    assert_not_nil assigns(:topic)
-    assert_equal true, assigns(:topic).subscribed?(users(:regular))
+    assert_equal true, @topic.subscribed?(@regular)
     assert_template "subscription"
     assert_response :success
   end
 
   test "should unsubscribe from notifications" do
-    login(users(:regular))
+    login(@regular)
     post subscription_topic_url(@topic, format: "js"), params: { notify: "0" }
-    assert_not_nil assigns(:topic)
-    assert_equal false, assigns(:topic).subscribed?(users(:regular))
+    assert_equal false, @topic.subscribed?(@regular)
     assert_template "subscription"
     assert_response :success
   end
@@ -43,32 +41,31 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    login(@regular_user)
+    login(@regular)
     get topics_url
     assert_response :success
-    assert_not_nil assigns(:topics)
   end
 
   test "should get new" do
-    login(@regular_user)
+    login(@regular)
     get new_topic_url
     assert_response :success
   end
 
   test "should create topic" do
-    login(@regular_user)
+    login(@regular)
     assert_difference("Topic.count") do
       post topics_url, params: { topic: topic_params }
     end
-    assert_not_nil assigns(:topic)
-    assert_equal "New Topic Title", assigns(:topic).title
-    assert_equal "This is the my new forum topic.", assigns(:topic).replies.first.description
-    assert_equal users(:user_1), assigns(:topic).user
-    assert_redirected_to assigns(:topic)
+    topic = Topic.last
+    assert_equal "New Topic Title", topic.title
+    assert_equal "This is the my new forum topic.", topic.replies.first.description
+    assert_equal @regular, topic.user
+    assert_redirected_to topic_url(topic)
   end
 
   test "should not create topic with blank title" do
-    login(@regular_user)
+    login(@regular)
     assert_difference("Topic.count", 0) do
       post topics_url, params: { topic: topic_params.merge(title: "") }
     end
@@ -83,11 +80,13 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest
         topic: {
           title: "http://www.example.com",
           slug: "http-www-example-com",
-          description: "http://www.example.com" }
+          description: "http://www.example.com"
         }
+      }
     end
-    assert_equal true, assigns(:topic).user.shadow_banned
-    assert_redirected_to assigns(:topic)
+    topic = Topic.last
+    assert_equal true, topic.user.shadow_banned
+    assert_redirected_to topic_url(topic)
   end
 
   test "should not shadow ban verified user" do
@@ -97,15 +96,17 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest
         topic: {
           title: "http://www.example.com",
           slug: "http-www-example-com",
-          description: "http://www.example.com" }
+          description: "http://www.example.com"
         }
+      }
     end
-    assert_equal false, assigns(:topic).user.shadow_banned
-    assert_redirected_to assigns(:topic)
+    topic = Topic.last
+    assert_equal false, topic.user.shadow_banned
+    assert_redirected_to topic_url(topic)
   end
 
   test "should show topic" do
-    login(@regular_user)
+    login(@regular)
     get topic_url(@topic)
     @topic.reload
     assert_equal 1, @topic.view_count
@@ -128,29 +129,31 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
-    login(@regular_user)
+    login(@regular)
     get edit_topic_url(@topic)
     assert_response :success
   end
 
   test "should update topic" do
-    login(@regular_user)
-    patch topic_url(@topic), params: { topic: topic_params.merge(title: "Updated Topic Title") }
+    login(@regular)
+    patch topic_url(@topic), params: {
+      topic: topic_params.merge(title: "Updated Topic Title")
+    }
     assert_redirected_to assigns(:topic)
   end
 
   test "should not update topic with blank title" do
-    login(@regular_user)
+    login(@regular)
     patch topic_url(@topic), params: { topic: topic_params.merge(title: "") }
     assert_template "edit"
     assert_response :success
   end
 
   test "should destroy topic" do
-    login(@regular_user)
+    login(@regular)
     assert_difference("Topic.current.count", -1) do
       delete topic_url(@topic)
     end
-    assert_redirected_to topics_path
+    assert_redirected_to topics_url
   end
 end
