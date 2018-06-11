f20d07109875bcef5b398a14b950169f72ea4155
Remo Mueller <remosm@gmail.com>
2018-06-11 14:55:24 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/admin/exports_controller_test.rb
+++ test/controllers/admin/exports_controller_test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to assure data can be exported by admins.
-class Admin::ExportsControllerTest < ActionController::TestCase
+class Admin::ExportsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @admin = users(:admin)
     @regular_user = users(:user_1)
@@ -20,101 +20,94 @@ class Admin::ExportsControllerTest < ActionController::TestCase
     }
   end
 
-  test 'should get index as admin' do
+  test "should get index as admin" do
     login(@admin)
-    get :index
-    assert_not_nil assigns(:admin_exports)
+    get admin_exports_url
     assert_response :success
   end
 
-  test 'should not get index as regular user' do
+  test "should not get index as regular user" do
     login(@regular_user)
-    get :index
-    assert_redirected_to root_path
+    get admin_exports_url
+    assert_redirected_to root_url
   end
 
-  test 'should create admin export as admin' do
+  test "should create admin export as admin" do
     login(@admin)
-    assert_difference('Admin::Export.count') do
-      post :create, params: { admin_export: admin_export_params }
+    assert_difference("Admin::Export.count") do
+      post admin_exports_url, params: { admin_export: admin_export_params }
     end
-
-    assert_redirected_to admin_export_path(assigns(:admin_export))
+    assert_redirected_to admin_export_url(Admin::Export.last)
   end
 
-  test 'should not create admin export as regular user' do
+  test "should not create admin export as regular user" do
     login(@regular_user)
-    assert_difference('Admin::Export.count', 0) do
-      post :create, params: { admin_export: admin_export_params }
+    assert_difference("Admin::Export.count", 0) do
+      post admin_exports_url, params: { admin_export: admin_export_params }
     end
-
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should show admin export as admin' do
+  test "should show admin export as admin" do
     login(@admin)
-    get :show, params: { id: @admin_export }
+    get admin_export_url(@admin_export)
     assert_response :success
   end
 
-  test 'should not show admin export as regular user' do
+  test "should not show admin export as regular user" do
     login(@regular_user)
-    get :show, params: { id: @admin_export }
-    assert_redirected_to root_path
+    get admin_export_url(@admin_export)
+    assert_redirected_to root_url
   end
 
-  test 'should get progress as admin' do
+  test "should get progress as admin" do
     login(@admin)
-    post :progress, params: { id: @admin_export }, format: 'js'
-    assert_template 'progress'
+    post progress_admin_export_url(@admin_export, format: "js")
+    assert_template "progress"
     assert_response :success
   end
 
-  test 'should not get progress as regular user' do
+  test "should not get progress as regular user" do
     login(@regular_user)
-    post :progress, params: { id: @admin_export }, format: 'js'
-    assert_redirected_to root_path
+    post progress_admin_export_url(@admin_export, format: "js")
+    assert_redirected_to root_url
   end
 
-  test 'should download export file as admin' do
+  test "should download export file as admin" do
     login(@admin)
     assert_not_equal 0, @completed.file.size
-    get :file, params: { id: @completed }
-    assert_not_nil assigns(:admin_export)
-    assert_not_nil response
-    assert_kind_of String, response.body
-    assert_equal File.binread(File.join(CarrierWave::Uploader::Base.root, assigns(:admin_export).file.url)), response.body
+    get file_admin_export_url(@completed)
+    assert_equal File.binread(@completed.file.path), response.body
   end
 
-  test 'should not download export file as regular user' do
+  test "should not download export file as regular user" do
     login(@regular_user)
-    get :file, params: { id: @completed }
-    assert_nil assigns(:admin_export)
-    assert_redirected_to root_path
+    get file_admin_export_url(@completed)
+    assert_redirected_to root_url
   end
 
-  test 'should destroy admin export as admin' do
+  test "should destroy admin export as admin" do
     login(@admin)
-    assert_difference('Admin::Export.count', -1) do
-      delete :destroy, params: { id: @admin_export }
+    assert_difference("Admin::Export.count", -1) do
+      delete admin_export_url(@admin_export)
     end
-    assert_redirected_to admin_exports_path
+    assert_redirected_to admin_exports_url
   end
 
-  test 'should destroy admin export using ajax' do
+  test "should destroy admin export using ajax" do
     login(@admin)
-    assert_difference('Admin::Export.count', -1) do
-      delete :destroy, params: { id: @admin_export }, format: 'js'
+    assert_difference("Admin::Export.count", -1) do
+      delete admin_export_url(@admin_export, format: "js")
     end
-    assert_template 'destroy'
+    assert_template "destroy"
     assert_response :success
   end
 
-  test 'should not destroy admin export as regular user' do
+  test "should not destroy admin export as regular user" do
     login(@regular_user)
-    assert_difference('Admin::Export.count', 0) do
-      delete :destroy, params: { id: @admin_export }
+    assert_difference("Admin::Export.count", 0) do
+      delete admin_export_url(@admin_export)
     end
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 end
