8771ac3a3e41fbaf4ea98475d079568de0b73c9b
Remo Mueller <remosm@gmail.com>
2018-06-11 15:13:38 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,199 +1,202 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
-# Tests access to upload and view images in blogs and forum posts
-class ImagesControllerTest < ActionController::TestCase
+# Tests access to upload and view images in blogs and forum posts.
+class ImagesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @admin = users(:admin)
+    @regular = users(:user_1)
     @image = images(:one)
   end
 
-  test 'should get index as admin' do
-    login(users(:admin))
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:images)
+  def fixture_rails_png
+    fixture_file_upload("../../test/support/images/rails.png")
   end
 
-  test 'should not get index as public user' do
-    get :index
-    assert_redirected_to new_user_session_path
+  def image_params
+    {
+      image: fixture_rails_png
+    }
   end
 
-  test 'should not get index as regular user' do
-    login(users(:user_1))
-    get :index
-    assert_redirected_to root_path
+  def multiple_images
+    [fixture_rails_png, fixture_rails_png]
   end
 
-  test 'should get new as admin' do
-    login(users(:admin))
-    get :new
+  test "should get index as admin" do
+    login(@admin)
+    get images_url
     assert_response :success
   end
 
-  test 'should get new as regular user' do
-    login(users(:user_1))
-    get :new
+  test "should not get index as public user" do
+    get images_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should not get index as regular user" do
+    login(@regular)
+    get images_url
+    assert_redirected_to root_url
+  end
+
+  test "should get new as admin" do
+    login(@admin)
+    get new_image_url
     assert_response :success
   end
 
-  test 'should not get new as public user' do
-    get :new
-    assert_redirected_to new_user_session_path
-  end
-
-  test 'should create image as admin' do
-    login(users(:admin))
-    assert_difference('Image.count') do
-      post :create, params: { image: { image: fixture_file_upload('../../test/support/images/rails.png') } }
-    end
-    assert_redirected_to image_path(assigns(:image))
-  end
-
-  test 'should create image as regular user' do
-    login(users(:user_1))
-    assert_difference('Image.count') do
-      post :create, params: { image: { image: fixture_file_upload('../../test/support/images/rails.png') } }
-    end
-    assert_redirected_to image_path(assigns(:image))
-  end
-
-  test 'should create image as public user' do
-    assert_difference('Image.count', 0) do
-      post :create, params: { image: { image: fixture_file_upload('../../test/support/images/rails.png') } }
-    end
-    assert_redirected_to new_user_session_path
-  end
-
-  test 'should upload multiple images as admin' do
-    login(users(:admin))
-    assert_difference('Image.count', 2) do
-      post :create_multiple, params: {
-        images: [fixture_file_upload('../../test/support/images/rails.png'), fixture_file_upload('../../test/support/images/rails.png')]
-      }, format: 'js'
-    end
-    assert_template 'create_multiple'
+  test "should get new as regular user" do
+    login(@regular)
+    get new_image_url
     assert_response :success
   end
 
-  test 'should upload multiple images as regular user' do
-    login(users(:user_1))
-    assert_difference('Image.count', 2) do
-      post :create_multiple, params: {
-        images: [fixture_file_upload('../../test/support/images/rails.png'), fixture_file_upload('../../test/support/images/rails.png')]
-      }, format: 'js'
+  test "should not get new as public user" do
+    get new_image_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should create image as admin" do
+    login(@admin)
+    assert_difference("Image.count") do
+      post images_url, params: { image: image_params }
     end
-    assert_template 'create_multiple'
+    assert_redirected_to image_url(assigns(:image))
+  end
+
+  test "should create image as regular user" do
+    login(@regular)
+    assert_difference("Image.count") do
+      post images_url, params: { image: image_params }
+    end
+    assert_redirected_to image_url(assigns(:image))
+  end
+
+  test "should create image as public user" do
+    assert_difference("Image.count", 0) do
+      post images_url, params: { image: image_params }
+    end
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should upload multiple images as admin" do
+    login(@admin)
+    assert_difference("Image.count", 2) do
+      post upload_images_url(format: "js"), params: { images: multiple_images }
+    end
+    assert_template "create_multiple"
     assert_response :success
   end
 
-  test 'should not upload multiple images as public user' do
-    assert_difference('Image.count', 0) do
-      post :create_multiple, params: {
-        images: [fixture_file_upload('../../test/support/images/rails.png'), fixture_file_upload('../../test/support/images/rails.png')]
-      }, format: 'js'
+  test "should upload multiple images as regular user" do
+    login(@regular)
+    assert_difference("Image.count", 2) do
+      post upload_images_url(format: "js"), params: { images: multiple_images }
+    end
+    assert_template "create_multiple"
+    assert_response :success
+  end
+
+  test "should not upload multiple images as public user" do
+    assert_difference("Image.count", 0) do
+      post upload_images_url(format: "js"), params: { images: multiple_images }
     end
     assert_response :unauthorized
   end
 
-  test 'should show image as admin' do
-    login(users(:admin))
-    get :show, params: { id: @image }
+  test "should show image as admin" do
+    login(@admin)
+    get image_url(@image)
     assert_response :success
   end
 
-  test 'should show image as regular user' do
-    login(users(:user_1))
-    get :show, params: { id: @image }
+  test "should show image as regular user" do
+    login(@regular)
+    get image_url(@image)
     assert_response :success
   end
 
-  test 'should show image as public user' do
-    get :show, params: { id: @image }
+  test "should show image as public user" do
+    get image_url(@image)
     assert_response :success
   end
 
-  test 'should download image as admin' do
-    login(users(:admin))
-    get :download, params: { id: @image }
-    assert_not_nil assigns(:image)
-    assert_kind_of String, response.body
-    assert_equal File.binread(File.join(CarrierWave::Uploader::Base.root, assigns(:image).image.url)), response.body
+  test "should download image as admin" do
+    login(@admin)
+    get download_image_url(@image)
+    assert_equal File.binread(assigns(:image).image.path), response.body
     assert_response :success
   end
 
-  test 'should download image as regular user' do
-    login(users(:user_1))
-    get :download, params: { id: @image }
-    assert_not_nil assigns(:image)
-    assert_kind_of String, response.body
-    assert_equal File.binread(File.join(CarrierWave::Uploader::Base.root, assigns(:image).image.url)), response.body
+  test "should download image as regular user" do
+    login(@regular)
+    get download_image_url(@image)
+    assert_equal File.binread(assigns(:image).image.path), response.body
     assert_response :success
   end
 
-  test 'should download image as public user' do
-    get :download, params: { id: @image }
-    assert_not_nil assigns(:image)
-    assert_kind_of String, response.body
-    assert_equal File.binread(File.join(CarrierWave::Uploader::Base.root, assigns(:image).image.url)), response.body
+  test "should download image as public user" do
+    get download_image_url(@image)
+    assert_equal File.binread(assigns(:image).image.path), response.body
     assert_response :success
   end
 
-  test 'should get edit as admin' do
-    login(users(:admin))
-    get :edit, params: { id: @image }
+  test "should get edit as admin" do
+    login(@admin)
+    get edit_image_url(@image)
     assert_response :success
   end
 
-  test 'should not get edit as regular user' do
-    login(users(:user_1))
-    get :edit, params: { id: @image }
-    assert_redirected_to root_path
+  test "should not get edit as regular user" do
+    login(@regular)
+    get edit_image_url(@image)
+    assert_redirected_to root_url
   end
 
-  test 'should not get edit as public user' do
-    get :edit, params: { id: @image }
-    assert_redirected_to new_user_session_path
+  test "should not get edit as public user" do
+    get edit_image_url(@image)
+    assert_redirected_to new_user_session_url
   end
 
-  test 'should update image as admin' do
-    login(users(:admin))
-    patch :update, params: { id: images(:three), image: { image: fixture_file_upload('../../test/support/images/rails.png') } }
-    assert_redirected_to image_path(assigns(:image))
+  test "should update image as admin" do
+    login(@admin)
+    patch image_url(images(:three)), params: { image: image_params }
+    assert_redirected_to image_url(images(:three))
   end
 
-  test 'should not update image as regular user' do
-    login(users(:user_1))
-    patch :update, params: { id: images(:three), image: { image: fixture_file_upload('../../test/support/images/rails.png') } }
-    assert_redirected_to root_path
+  test "should not update image as regular user" do
+    login(@regular)
+    patch image_url(images(:three)), params: { image: image_params }
+    assert_redirected_to root_url
   end
 
-  test 'should not update image as public user' do
-    patch :update, params: { id: images(:three), image: { image: fixture_file_upload('../../test/support/images/rails.png') } }
-    assert_redirected_to new_user_session_path
+  test "should not update image as public user" do
+    patch image_url(images(:three)), params: { image: image_params }
+    assert_redirected_to new_user_session_url
   end
 
-  test 'should destroy image as admin' do
-    login(users(:admin))
-    assert_difference('Image.count', -1) do
-      delete :destroy, params: { id: images(:three) }
+  test "should destroy image as admin" do
+    login(@admin)
+    assert_difference("Image.count", -1) do
+      delete image_url(images(:three))
     end
-    assert_redirected_to images_path
+    assert_redirected_to images_url
   end
 
-  test 'should not destroy image as regular user' do
-    login(users(:user_1))
-    assert_difference('Image.count', 0) do
-      delete :destroy, params: { id: images(:three) }
+  test "should not destroy image as regular user" do
+    login(@regular)
+    assert_difference("Image.count", 0) do
+      delete image_url(images(:three))
     end
-    assert_redirected_to root_path
+    assert_redirected_to root_url
   end
 
-  test 'should not destroy image as public user' do
-    assert_difference('Image.count', 0) do
-      delete :destroy, params: { id: images(:three) }
+  test "should not destroy image as public user" do
+    assert_difference("Image.count", 0) do
+      delete image_url(images(:three))
     end
-    assert_redirected_to new_user_session_path
+    assert_redirected_to new_user_session_url
   end
 end
