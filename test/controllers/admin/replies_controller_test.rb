41a5c7fd03bc87b8ad1c1f6a46f355d99c92b912
Remo Mueller <remosm@gmail.com>
2018-06-11 15:43:44 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/admin/replies_controller_test.rb
+++ test/controllers/admin/replies_controller_test.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to assure that admins can review forum replies.
-class Admin::RepliesControllerTest < ActionController::TestCase
-  test 'should get index' do
-    login(users(:admin))
-    get :index
+class Admin::RepliesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = users(:admin)
+  end
+
+  test "should get index" do
+    login(@admin)
+    get admin_replies_url
     assert_response :success
   end
 end
