782d750d95af0baa9fab519d7cd3c34de9bf2386
Remo Mueller <remosm@gmail.com>
2018-06-11 15:18:34 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -3,64 +3,61 @@
 require "test_helper"
 
 # Test that notifications can be viewed and marked as read.
-class NotificationsControllerTest < ActionController::TestCase
+class NotificationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @regular_user = users(:user_1)
   end
 
   test "should get index" do
     login(@regular_user)
-    get :index
+    get notifications_url
     assert_response :success
-    assert_not_nil assigns(:notifications)
   end
 
   test "should get all read index" do
     login(@regular_user)
-    get :index, params: { all: "1" }
+    get notifications_url, params: { all: "1" }
     assert_response :success
-    assert_not_nil assigns(:notifications)
   end
 
   test "should show blog post reply notification" do
     login(@regular_user)
-    get :show, params: { id: notifications(:broadcast_reply_two) }
-    assert_not_nil assigns(:notification)
-    assert_equal true, assigns(:notification).read
+    get notification_url(notifications(:broadcast_reply_two))
+    notifications(:broadcast_reply_two).reload
+    assert_equal true, notifications(:broadcast_reply_two).read?
     assert_redirected_to notifications(:broadcast_reply_two).reply
   end
 
   test "should show blank notification and redirect" do
     login(@regular_user)
-    get :show, params: { id: notifications(:blank) }
-    assert_not_nil assigns(:notification)
-    assert_equal true, assigns(:notification).read
-    assert_redirected_to notifications_path
+    get notification_url(notifications(:blank))
+    notifications(:blank).reload
+    assert_equal true, notifications(:blank).read?
+    assert_redirected_to notifications_url
   end
 
   test "should not show notification without valid id" do
     login(@regular_user)
-    get :show, params: { id: -1 }
-    assert_nil assigns(:notification)
-    assert_redirected_to notifications_path
+    get notification_url(-1)
+    assert_redirected_to notifications_url
   end
 
   test "should update notification" do
     login(@regular_user)
-    patch :update, params: {
-      id: notifications(:broadcast_reply_two), notification: { read: true }
-    }, format: "js"
-    assert_not_nil assigns(:notification)
-    assert_equal true, assigns(:notification).read
+    patch notification_url(notifications(:broadcast_reply_two), format: "js"), params: {
+      notification: { read: true }
+    }
+    notifications(:broadcast_reply_two).reload
+    assert_equal true, notifications(:broadcast_reply_two).read?
     assert_template "show"
     assert_response :success
   end
 
   test "should mark all as read" do
     login(@regular_user)
-    patch :mark_all_as_read, params: {
+    patch mark_all_as_read_notifications_url(format: "js"), params: {
       broadcast_id: broadcasts(:published).id
-    }, format: "js"
+    }
     assert_equal 0, @regular_user.notifications.where(broadcast_id: broadcasts(:published), read: false).count
     assert_template "mark_all_as_read"
     assert_response :success
@@ -69,7 +66,7 @@ class NotificationsControllerTest < ActionController::TestCase
   test "should not mark all as read without broadcast or topic id" do
     login(@regular_user)
     assert_difference("Notification.where(read: false).count", 0) do
-      patch :mark_all_as_read, format: "js"
+      patch mark_all_as_read_notifications_url(format: "js")
     end
     assert_template "mark_all_as_read"
     assert_response :success
