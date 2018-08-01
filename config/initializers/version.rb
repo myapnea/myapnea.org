cc4d4790d69e31c1c760cb9ca5cca3657a651bfe
Remo Mueller <remosm@gmail.com>
2018-08-01 17:49:07 -0400

    Version bump to 18.0.0.beta6

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module Myapnea
     MAJOR = 18
     MINOR = 0
     TINY = 0
-    BUILD = "beta5" # "pre", "beta1", "beta2", "rc", "rc2", nil
+    BUILD = "beta6" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
