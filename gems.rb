e5c7e1d01879c77ff9c306a0c6d25670393a735c
Remo Mueller <remosm@gmail.com>
2018-10-09 09:54:47 -0400

    Remove capybara and selenium-webdriver from development environment

--- gems.rb
+++ gems.rb
@@ -33,19 +33,16 @@ gem "jbuilder",             "~> 2.5"
 gem "jquery-rails",         "~> 4.3.3"
 gem "turbolinks",           "~> 5"
 
-# Testing
-group :test do
-  gem "artifice"
-  gem "minitest"
-  gem "puma"
-  gem "simplecov",          "~> 0.16.1", require: false
-end
-
 group :development do
   gem "web-console", "~> 3.0"
 end
 
-group :development, :test do
-  gem "capybara", "~> 3.0"
+# Testing
+group :test do
+  gem "artifice"
+  gem "capybara",           ">= 2.15", "< 4.0"
+  gem "minitest"
+  gem "puma"
   gem "selenium-webdriver"
+  gem "simplecov",          "~> 0.16.1", require: false
 end
